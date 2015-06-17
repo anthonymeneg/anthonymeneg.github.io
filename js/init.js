be78e7a51ba1d017bd76a1034b0f157f1c0d1d12
Istvan Pusztai <istvanp@gmail.com>
2015-06-17 01:58:54 -0400

    Animation fixes and add Maison Mémé section

--- js/init.js
+++ js/init.js
@@ -13,8 +13,32 @@ $.stellar({
     positionProperty: 'transform',
 });
 
+jQuery.extend(jQuery.easing, {
+    easeInOutCirc: function (x, t, b, c, d) {
+        if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+        return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+    }
+});
+
 $(function() {
-    var content2_1 = new Waypoint({
+    $('#logo').on('click', function() {
+        $('html, body').stop().animate({
+        scrollTop: 0
+    }, 1000, 'easeInOutCirc');
+        return false;
+    });
+
+    var content2_0 = new Waypoint({
+        element: document.getElementById('content2'),
+        handler: function(direction) {
+            if (direction == 'down') {
+                $('#logo').addClass('active');
+            } else {
+                $('#logo').removeClass('active');
+            }
+        },
+        offset: '50%'
+    }), content2_1 = new Waypoint({
         element: document.getElementById('content-2-animations'),
         handler: function(direction) {
             $(this.element).find('.paused').removeClass('paused').addClass('animate');
